edf4baf6d94b9c966df2da1866095c80526f51e0
teodesson <teodesson@gmail.com>
2019-10-15 01:10:13 +0800

    intial commit

---
+++ src/main/java/com/dariawan/contactapp/ContactAppApplication.java
@@ -0,0 +1,13 @@
+package com.dariawan.contactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAppApplication.class, args);
+	}
+
+}
