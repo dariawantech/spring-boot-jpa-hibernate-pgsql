26bf2aeb92832c4242cf402085e2fafff5bddeb7
teodesson <teodesson@gmail.com>
2020-05-03 02:46:17 +0800

    To fix NumberFormatException: For input string: "" in Swagger, upgrade Spring Boot version

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Spring Boot + JPA/Hibernate + PostgreSQL RESTful CRUD API Example with SpringFox
 Source codes from articles:
 * [Spring Boot + JPA/Hibernate + PostgreSQL RESTful CRUD API Example](https://www.dariawan.com/tutorials/spring/spring-boot-jpa-hibernate-postgresql-restful-crud-api-example/)
 * [Documenting Spring Boot REST API with Swagger](https://www.dariawan.com/tutorials/spring/documenting-spring-boot-rest-api-swagger/)
+* [NumberFormatException: For input string: "" in Swagger](https://www.dariawan.com/tutorials/rest/numberformatexception-for-input-string-in-swagger/)
--- src/main/java/com/dariawan/contactapp/controller/ContactController.java
+++ src/main/java/com/dariawan/contactapp/controller/ContactController.java
@@ -4,37 +4,32 @@
  *
  * Creative Commons Attribution-ShareAlike 4.0 International License
  *
- * Under this license, you are free to:
- * # Share - copy and redistribute the material in any medium or format
- * # Adapt - remix, transform, and build upon the material for any purpose,
- *   even commercially.
+ * Under this license, you are free to: # Share - copy and redistribute the
+ * material in any medium or format # Adapt - remix, transform, and build upon
+ * the material for any purpose, even commercially.
  *
- * The licensor cannot revoke these freedoms
- * as long as you follow the license terms.
+ * The licensor cannot revoke these freedoms as long as you follow the license
+ * terms.
  *
- * License terms:
- * # Attribution - You must give appropriate credit, provide a link to the
- *   license, and indicate if changes were made. You may do so in any
- *   reasonable manner, but not in any way that suggests the licensor
- *   endorses you or your use.
- * # ShareAlike - If you remix, transform, or build upon the material, you must
- *   distribute your contributions under the same license as the original.
- * # No additional restrictions - You may not apply legal terms or
- *   technological measures that legally restrict others from doing anything the
- *   license permits.
+ * License terms: # Attribution - You must give appropriate credit, provide a
+ * link to the license, and indicate if changes were made. You may do so in any
+ * reasonable manner, but not in any way that suggests the licensor endorses you
+ * or your use. # ShareAlike - If you remix, transform, or build upon the
+ * material, you must distribute your contributions under the same license as
+ * the original. # No additional restrictions - You may not apply legal terms or
+ * technological measures that legally restrict others from doing anything the
+ * license permits.
  *
- * Notices:
- * # You do not have to comply with the license for elements of the material in
- *   the public domain or where your use is permitted by an applicable exception
- *   or limitation.
- * # No warranties are given. The license may not give you all of
- *   the permissions necessary for your intended use. For example, other rights
- *   such as publicity, privacy, or moral rights may limit how you use
- *   the material.
+ * Notices: # You do not have to comply with the license for elements of the
+ * material in the public domain or where your use is permitted by an applicable
+ * exception or limitation. # No warranties are given. The license may not give
+ * you all of the permissions necessary for your intended use. For example,
+ * other rights such as publicity, privacy, or moral rights may limit how you
+ * use the material.
  *
  * You may obtain a copy of the License at
- *   https://creativecommons.org/licenses/by-sa/4.0/
- *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ * https://creativecommons.org/licenses/by-sa/4.0/
+ * https://creativecommons.org/licenses/by-sa/4.0/legalcode
  */
 package com.dariawan.contactapp.controller;
 
@@ -76,36 +71,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ContactController {
-    
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
     private final int ROW_PER_PAGE = 5;
-    
+
     @Autowired
     private ContactService contactService;
-    
-    @ApiOperation(value = "Find Contacts by name", notes = "Name search by %name% format", tags = { "contact" })
+
+    @ApiOperation(value = "Find Contacts by name", notes = "Name search by %name% format", tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "successful operation", response=List.class )  })	    
+        @ApiResponse(code = 200, message = "successful operation", response = List.class)})
     @GetMapping(value = "/contacts")
     public ResponseEntity<List<Contact>> findAll(
-            @ApiParam("Page number, default is 1") @RequestParam(value="page", defaultValue="1") int pageNumber,
-            @ApiParam("Name of the contact for search.") @RequestParam(required=false) String name) {
+            @ApiParam(name = "contactId",
+                    value = "Page number, default is 1",
+                    example = "1",
+                    required = false) @RequestParam(value = "page", defaultValue = "1") int pageNumber,
+            @ApiParam("Name of the contact for search.") @RequestParam(required = false) String name) {
         if (StringUtils.isEmpty(name)) {
             return ResponseEntity.ok(contactService.findAll(pageNumber, ROW_PER_PAGE));
-        }
-        else {
+        } else {
             return ResponseEntity.ok(contactService.findAllByName(name, pageNumber, ROW_PER_PAGE));
         }
     }
 
-    @ApiOperation(value = "Find contact by ID", notes = "Returns a single contact", tags = { "contact" })
+    @ApiOperation(value = "Find contact by ID", notes = "Returns a single contact", tags = {"contact"})
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "successful operation", response=Contact.class),
-        @ApiResponse(code = 404, message = "Contact not found") })
+        @ApiResponse(code = 200, message = "successful operation", response = Contact.class),
+        @ApiResponse(code = 404, message = "Contact not found")})
     @GetMapping(value = "/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Contact> findContactById(
-            @ApiParam("Id of the contact to be obtained. Cannot be empty.")
+            @ApiParam(name = "contactId",
+                    value = "Id of the contact to be obtained. Cannot be empty.",
+                    example = "1",
+                    required = true)
             @PathVariable long contactId) {
         try {
             Contact contact = contactService.findById(contactId);
@@ -114,16 +114,16 @@ public class ContactController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // return 404, with null body
         }
     }
-    
-    @ApiOperation(value = "Add a new contact", tags = { "contact" })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 201, message = "Contact created"), 
-        @ApiResponse(code = 400, message = "Invalid input"), 
-        @ApiResponse(code = 409, message = "Contact already exists") })	    
+
+    @ApiOperation(value = "Add a new contact", tags = {"contact"})
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Contact created"),
+        @ApiResponse(code = 400, message = "Invalid input"),
+        @ApiResponse(code = 409, message = "Contact already exists")})
     @PostMapping(value = "/contacts")
     public ResponseEntity<Contact> addContact(
             @ApiParam("Contact to add. Cannot null or empty.")
-            @Valid @RequestBody Contact contact) 
+            @Valid @RequestBody Contact contact)
             throws URISyntaxException {
         try {
             Contact newContact = contactService.save(contact);
@@ -139,16 +139,19 @@ public class ContactController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
-    
-    @ApiOperation(value = "Update an existing contact", tags = { "contact" })
+
+    @ApiOperation(value = "Update an existing contact", tags = {"contact"})
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "successful operation"),
         @ApiResponse(code = 400, message = "Invalid ID supplied"),
         @ApiResponse(code = 404, message = "Contact not found"),
-        @ApiResponse(code = 405, message = "Validation exception") })
+        @ApiResponse(code = 405, message = "Validation exception")})
     @PutMapping(value = "/contacts/{contactId}")
     public ResponseEntity<Contact> updateContact(
-            @ApiParam("Id of the contact to be update. Cannot be empty.")
+            @ApiParam(name = "contactId",
+                    value = "Id of the contact to be update. Cannot be empty.",
+                    example = "1",
+                    required = true)
             @PathVariable long contactId,
             @ApiParam("Contact to update. Cannot null or empty.")
             @Valid @RequestBody Contact contact) {
@@ -166,14 +169,17 @@ public class ContactController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
-    
-    @ApiOperation(value = "Update an existing contact's address", tags = { "contact" })
+
+    @ApiOperation(value = "Update an existing contact's address", tags = {"contact"})
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "successful operation"),
-        @ApiResponse(code = 404, message = "Contact not found") })
+        @ApiResponse(code = 404, message = "Contact not found")})
     @PatchMapping("/contacts/{contactId}")
     public ResponseEntity<Void> updateAddress(
-            @ApiParam("Id of the contact to be update. Cannot be empty.")
+            @ApiParam(name = "contactId",
+                    value = "Id of the contact to be update. Cannot be empty.",
+                    example = "1",
+                    required = true)
             @PathVariable long contactId,
             @ApiParam("Contact's address to update.")
             @RequestBody Address address) {
@@ -186,14 +192,17 @@ public class ContactController {
             return ResponseEntity.notFound().build();
         }
     }
-    
-    @ApiOperation(value = "Deletes a contact", tags = { "contact" })
-    @ApiResponses(value = { 
+
+    @ApiOperation(value = "Deletes a contact", tags = {"contact"})
+    @ApiResponses(value = {
         @ApiResponse(code = 200, message = "successful operation"),
-        @ApiResponse(code = 404, message = "Contact not found") })
-    @DeleteMapping(path="/contacts/{contactId}")
+        @ApiResponse(code = 404, message = "Contact not found")})
+    @DeleteMapping(path = "/contacts/{contactId}")
     public ResponseEntity<Void> deleteContactById(
-            @ApiParam("Id of the contact to be delete. Cannot be empty.")
+            @ApiParam(name = "contactId",
+                    value = "Id of the contact to be delete. Cannot be empty.",
+                    example = "1",
+                    required = true)
             @PathVariable long contactId) {
         try {
             contactService.deleteById(contactId);
